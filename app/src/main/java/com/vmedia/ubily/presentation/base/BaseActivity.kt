f9b00d6aaa6d5788cf9350da42945208779245e4
Denis Perfomer <perfomeria@gmail.com>
2020-04-04 13:30:24 +0700

    feature(all): Design things added (fonts, colors, drawables)

--- app/src/main/java/com/vmedia/ubily/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/vmedia/ubily/presentation/base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.vmedia.ubily.presentation.base
 
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +27,12 @@ abstract class BaseActivity(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
         setContentView(screenLayoutResource)
 
         if (savedInstanceState == null) {
