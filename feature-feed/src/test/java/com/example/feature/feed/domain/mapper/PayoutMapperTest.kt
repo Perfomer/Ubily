a011dd74af3202e46621edde290b2b9dcc3cccc2
Denis Perfomer <perfomeria@gmail.com>
2020-04-09 10:04:48 +0700

    test(feed): PayoutMapper tests added

---
+++ feature-feed/src/test/java/com/example/feature/feed/domain/mapper/PayoutMapperTest.kt
@@ -0,0 +1,76 @@
+package com.example.feature.feed.domain.mapper
+
+import com.example.feature.feed.domain.FeedRepository
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.vmedia.core.common.obj.EventType
+import com.vmedia.core.common.obj.Month.APRIL
+import com.vmedia.core.common.obj.of
+import com.vmedia.core.data.internal.database.entity.Event
+import com.vmedia.core.data.internal.database.entity.Payout
+import io.reactivex.Single
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.math.BigDecimal
+
+@RunWith(MockitoJUnitRunner::class)
+internal class PayoutMapperTest {
+
+    private val repository: FeedRepository = mock {
+        on { getPeriod(any()) } doReturn Single.just(MOCKY_PERIOD)
+    }
+
+    private val mapper: PayoutMapper
+        get() = PayoutMapper(repository)
+
+    @Test
+    fun map_empty() {
+        // Given
+        val error = Throwable()
+        whenever(repository.getEventPayout(any())).thenReturn(Single.error(error))
+
+        // When
+        val mapping = mapper.map(MOCKY_EVENT)
+
+        // Then
+        mapping.test()
+            .assertError(error)
+            .dispose()
+    }
+
+    @Test
+    fun map() {
+        // Given
+        whenever(repository.getEventPayout(any())).thenReturn(Single.just(MOCKY_PAYOUT))
+
+        // When
+        val mapping = mapper.map(MOCKY_EVENT)
+
+        // Then
+        mapping.test()
+            .assertComplete()
+            .assertValue { it.id == MOCKY_EVENT.id }
+            .assertValue { it.date == MOCKY_EVENT.date }
+            .assertValue { it.content.amount == MOCKY_PAYOUT.valueUsd }
+            .dispose()
+    }
+
+    private companion object {
+
+        private const val PERIODS_COUNT = 10
+
+        private val MOCKY_EVENT = Event(type = EventType.PAYOUT)
+
+        private val MOCKY_PAYOUT = Payout(
+            periodId = 1L,
+            valueUsd = BigDecimal(20)
+        )
+
+        private val MOCKY_PERIOD = APRIL of 2020
+
+    }
+
+}
