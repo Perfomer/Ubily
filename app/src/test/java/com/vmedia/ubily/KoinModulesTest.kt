c63fc703cfe69ba2812ca30dd8f73e580e9b877a
Denis Perfomer <perfomeria@gmail.com>
2020-04-09 08:56:17 +0700

    fix(app): KoinModulesTest fixed

--- app/src/test/java/com/vmedia/ubily/KoinModulesTest.kt
+++ app/src/test/java/com/vmedia/ubily/KoinModulesTest.kt
@@ -1,12 +1,18 @@
 package com.vmedia.ubily
 
+import android.os.Build
 import org.junit.Test
 import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
 import org.koin.test.KoinTest
 import org.koin.test.category.CheckModuleTest
 import org.koin.test.check.checkModules
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @Category(CheckModuleTest::class)
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class KoinModulesTest : KoinTest {
 
     @Test
