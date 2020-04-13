c6b8217a7bcb321d2e9266003683eb702e44f592
Denis Perfomer <perfomeria@gmail.com>
2020-04-14 01:51:06 +0700

    feature(app): WindowInsets handled in BaseActivity

--- app/src/main/java/com/vmedia/ubily/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/vmedia/ubily/presentation/base/BaseActivity.kt
@@ -1,11 +1,18 @@
 package com.vmedia.ubily.presentation.base
 
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.updatePadding
+import com.vmedia.core.common.util.doOnApplyWindowInsets
 import com.vmedia.core.navigation.ScreenDestination
+import com.vmedia.ubily.R
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import ru.terrakok.cicerone.NavigatorHolder
@@ -26,14 +33,42 @@ abstract class BaseActivity(
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.apply {
+                decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    decorView.systemUiVisibility = decorView.systemUiVisibility or
+                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                }
+
+                statusBarColor = ContextCompat.getColor(context, R.color.black_20)
+                navigationBarColor = ContextCompat.getColor(context, R.color.white_50)
+            }
+        }
+
         super.onCreate(savedInstanceState)
-
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-
         setContentView(screenLayoutResource)
+
+        val frameLayout = findViewById<FrameLayout>(frameLayoutResource)
+        frameLayout.doOnApplyWindowInsets { view, insets, initialPadding ->
+            view.updatePadding(
+                left = initialPadding.left + insets.systemWindowInsetLeft,
+                right = initialPadding.right + insets.systemWindowInsetRight
+            )
+
+            insets.replaceSystemWindowInsets(
+                Rect(
+                    0,
+                    insets.systemWindowInsetTop,
+                    0,
+                    insets.systemWindowInsetBottom
+                )
+            )
+        }
+
 
         if (savedInstanceState == null) {
             router.newRootScreen(startScreen)
