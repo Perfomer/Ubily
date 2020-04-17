4317da6402683209cb990146ac04a3b9e7a4decb
Denis Perfomer <perfomeria@gmail.com>
2020-04-17 21:54:08 +0700

    feature(app): Timber enabled

--- app/src/main/java/com/vmedia/ubily/UbilyApplication.kt
+++ app/src/main/java/com/vmedia/ubily/UbilyApplication.kt
@@ -6,6 +6,7 @@ import com.facebook.stetho.Stetho
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class UbilyApplication : Application() {
 
@@ -14,6 +15,8 @@ class UbilyApplication : Application() {
         super.onCreate()
 
         Stetho.initializeWithDefaults(this);
+
+        Timber.plant(Timber.DebugTree())
 
         startKoin {
             if (BuildConfig.DEBUG) androidLogger()
