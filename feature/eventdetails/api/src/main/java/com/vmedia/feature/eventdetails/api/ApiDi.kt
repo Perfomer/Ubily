6f227a2fcb60e9d4aa244a6b5ec70cd49d7a076c
Denis Perfomer <perfomeria@gmail.com>
2020-04-19 11:09:42 +0700

    fix(all): Build stability fixed

--- feature/eventdetails/api/src/main/java/com/vmedia/feature/eventdetails/api/ApiDi.kt
+++ feature/eventdetails/api/src/main/java/com/vmedia/feature/eventdetails/api/ApiDi.kt
@@ -1,6 +1,6 @@
 package com.vmedia.feature.eventdetails.api
 
+import com.vmedia.feature.eventdetails.domain.di.domainModule
 import com.vmedia.feature.eventdetails.presentation.di.presentationModule
-import com.vmedia.feature.sync.domain.di.domainModule
 
 val featureEventDetailsModules = domainModule + presentationModule
---
+++ feature/eventdetails/domain/src/main/java/com/vmedia/feature/eventdetails/domain/di/DomainDi.kt
@@ -0,0 +1,7 @@
+package com.vmedia.feature.eventdetails.domain.di
+
+import org.koin.dsl.module
+
+val domainModule = module {
+
+}
