838d3b91114ca5680258eb00e07bbc4881b8623d
d.balchugov <d.balchugov@tinkoff.ru>
2021-01-08 02:10:42 +0700

    refactoring(auth): Urls extracted to BuildConfig

--- feature/auth/data/src/main/java/com/vmedia/feature/auth/data/browser/UnitySignInCompletable.kt
+++ feature/auth/data/src/main/java/com/vmedia/feature/auth/data/browser/UnitySignInCompletable.kt
@@ -1,6 +1,7 @@
 package com.vmedia.feature.auth.data.browser
 
 import android.webkit.WebView
+import com.vmedia.core.common.android.BuildConfig
 import com.vmedia.feature.auth.data.browser.controller.AuthController
 import com.vmedia.feature.auth.data.browser.controller.invokeController
 import com.vmedia.feature.auth.domain.SignInTask
@@ -20,22 +21,19 @@ internal class UnityWebViewSignInTask(private val webView: WebView) : SignInTask
 private class UnitySignInCompletable(
     webView: WebView,
     private val authController: AuthController
-) : BrowserRequestCompletable(webView, PACKAGES_URL) {
+) : BrowserRequestCompletable(webView, BuildConfig.NETWORK_AUTHORIZED_URL) {
 
     override fun onPageLoaded(url: String) {
         when {
             url.isPackagesPageUrl() -> observer?.onComplete()
             url.isAuthPageUrl() -> webView.invokeController(authController)
+            else -> observer?.onError(IllegalStateException())
         }
     }
 
     private companion object {
-
-        private const val PACKAGES_URL = "https://publisher.assetstore.unity3d.com/packages.html"
-        private const val LOGIN_URL_PART = "id.unity.com"
-
-        private fun String.isPackagesPageUrl() = equals(PACKAGES_URL)
-        private fun String.isAuthPageUrl() = contains(LOGIN_URL_PART)
+        private fun String.isPackagesPageUrl() = equals(BuildConfig.NETWORK_AUTHORIZED_URL)
+        private fun String.isAuthPageUrl() = contains(BuildConfig.NETWORK_LOGIN_URL_PART)
 
     }
 
