d6569a961b3584f852c05293784452d240891c70
Denis Perfomer <perfomeria@gmail.com>
2021-01-10 04:17:28 +0700

    fix(common): CollapsibleToolbar is open now

--- core/common/android/src/main/java/com/vmedia/core/common/android/view/CollapsibleToolbar.kt
+++ core/common/android/src/main/java/com/vmedia/core/common/android/view/CollapsibleToolbar.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.google.android.material.appbar.AppBarLayout
 
-class CollapsibleToolbar @JvmOverloads constructor(
+open class CollapsibleToolbar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
